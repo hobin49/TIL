8d27fdea9ffd0806af55ee2b86f695da47c8867f
hobin49 <hobin505@gmail.com>
2022-07-11 18:41:23 +0900

    codeup15 ~ 25

---
+++ codeup/codeup15~25.py
@@ -0,0 +1,51 @@
+# 코드업 6015번 문제
+
+a, b = input().split()
+a=int(a)
+b=int(b)
+print(a)
+print(b)
+
+# 코드업 6016번 문제
+a, b = input().split()
+print(b, a)
+
+#코드업 6017번 문제
+s = input()
+print(s, s, s)
+
+# 코드업 6018번 문제
+a, b = input().split(":")
+print(a, b, sep= ':')
+
+# 코드업 6019번 문제
+y, m, d = input().split('.')
+print(d,m,y, sep= "-")
+
+# 코드업 6020번 문제
+YYMMDD,passpor = input().split('-')
+
+print(YYMMDD,passpor, sep= "")
+
+# 코드업 6021번 문제
+c = input()
+print(c[0])
+print(c[1])
+print(c[2])
+print(c[3])
+print(c[4])
+
+# 코드업 6022번 문제
+c = input()
+print(c[0:2], c[2:4], c[4:6])
+
+# 코드업 6023번 문제
+c = input().split(":")
+print(c[1])
+
+# 코드업 6024번 문제
+w1, w2 = input().split()
+s = w1 + w2
+print(s)
+
+# 코드업 
